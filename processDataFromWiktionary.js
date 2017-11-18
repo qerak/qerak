23629e7270208c2b088363cc04aa9a0d0f14fde8
ashotjanibekyan <ashotjanibekyan@gmail.com>
2017-11-18 04:03:43 +0400

    փորձ

--- processDataFromWiktionary.js
+++ processDataFromWiktionary.js
@@ -1,4 +1,16 @@
-const processDataFromWiktionary = function(rawData) {
-    let processedData = rawData;
+const processDataFromWiktionary = function(word) {
+    const processedData = {};
+    const url = "https://hy.wiktionary.org/w/api.php?";
+    $.getJSON(url, {
+        "action": "parse",
+        "format": "json",
+        "page": word,
+        "prop": "text|categories|links|externallinks|sections",
+        "wrapoutputclass": "mw-parser-output",
+        "disablelimitreport": 1,
+        "disableeditsection": 1
+    }, function(data) {
+        processedData.output = data;
+    }); 
     return processedData;
 };

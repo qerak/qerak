533d92ef96f260073f52ccfaa6e690782f530d54
ashotjanibekyan <ashotjanibekyan@gmail.com>
2017-11-19 06:09:16 +0400

    վեջնական կապ, սրանից երևի թե էսքանով պրծնենք API-ի հետ գործ ունենալուց

--- background.js
+++ background.js
@@ -7,10 +7,9 @@ const contextMenuItem = {
 chrome.contextMenus.create(contextMenuItem);
 
 chrome.contextMenus.onClicked.addListener(function(clickedData) {
-    console.log(document);
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-          console.log(response.farewell);
+        chrome.tabs.sendMessage(tabs[0].id, clickedData.selectionText, function(response) {
+          console.log(response);
         });
       });
 })
